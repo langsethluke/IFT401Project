9e53114aedead0bd2822a3279ebb93dc66c400c6
langsethluke <166647046+langsethluke@users.noreply.github.com>
2024-10-02 20:35:47 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/trade')
+def trade():
+    return render_template('trade.html')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portoflio.html')
+
+@app.route('/tradinghistory')
+def tradinghistory():
+    return render_template('tradinghistory.html')
+
+@app.route('/accounttransfer')
+def accounttransfer():
+    return render_template('accounttransfer.html')
+
+if __name__ == "__main__": app.run(debug=True)
